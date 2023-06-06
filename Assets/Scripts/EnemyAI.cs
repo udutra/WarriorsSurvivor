cb5b8295b43b9074a3bf4d0e4f005289036efce1
Guilherme Galarça Dutra <udutra@gmail.com>
2023-06-06 17:36:36 -0300

    PROGRAMANDO A MOVIMENTAÇÃO E COMPORAMENTO DE PERSEGUIÇÃO DOS INIMIGOS.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,7 +1,32 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour {
+
+    private Vector3 target;
+    private bool isLookLeft;
+    private Rigidbody2D _rigidbody;
+    private Vector2 moveDirection;
+    private float currentHealth;
+    private float knokBackTime;
+    private int knokBackFactor = 1;
+    public GameObject hitArea;
     public EnemyData enemyData;
+
+    private void Start() {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        StartCoroutine(nameof(IETargetUpdate));
+    }
+
+    private void Update() {
+        _rigidbody.velocity = moveDirection.normalized * enemyData.moveSpeed * knokBackFactor;
+    }
+
+    private IEnumerator IETargetUpdate() {
+        while (true) {
+            yield return new WaitForSeconds(enemyData.targetUpdateDelay);
+            target = Core.Instance.gameManager.player.position;
+            moveDirection = target - transform.position;
+        }
+    }
 }
--- Assets/Scripts/Scriptables/EnemyData.cs
+++ Assets/Scripts/Scriptables/EnemyData.cs
@@ -2,4 +2,9 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Enemy", menuName = "Scriptable/Enemy", order = 2)]
 public class EnemyData : ScriptableObject {
+
+    public float targetUpdateDelay;
+    public float moveSpeed;
+
+
 }
