06e00550f9d176b1ab347119749139a2bc71a0af
Guilherme Galarça Dutra <udutra@gmail.com>
2023-06-16 22:03:55 -0300

    24 - APLICANDO BÔNUS DE UPGRADE DE GAMAPLAY.

--- Assets/Scripts/UpgradeManager.cs
+++ Assets/Scripts/UpgradeManager.cs
@@ -1,6 +1,16 @@
+using System.Collections.Generic;
 using UnityEngine;
 
+
 public class UpgradeManager : MonoBehaviour {
+
+    public PowerUpData powerUpTemp;
+    public int level;
+    public Dictionary<PowerUpData, int> passiveBonus = new();
+
+    private void Start() {
+        passiveBonus.Add(powerUpTemp, level);
+    }
 
     public float GetPowerUpBonus(PowerUpData powerUp) {
         float bonus = 0;
@@ -11,7 +21,9 @@ public class UpgradeManager : MonoBehaviour {
         }
 
         //Pegar o bonus de upgrades da Gameplay
-
+        if(passiveBonus.ContainsKey(powerUp)) {
+            bonus += powerUp.passive.value * passiveBonus[powerUp];
+        }
 
         //Pegar o bonus inerente ao Herois
         if (Core.Instance.gameManager.selectedHero.GetPowerUpBonus(powerUp) > 0) {
